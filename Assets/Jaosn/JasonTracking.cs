e91444a286521fbb3186445079fc15a9c0b0ee6e
鄧紹偉 <shaowei_teng@arplanet.com.tw>
2021-08-07 11:05:19 +0800

    update

---
+++ Assets/Jaosn/JasonTracking.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Microsoft.MixedReality.Toolkit.Input;
+public class JasonTracking : MonoBehaviour, IMixedRealityFocusHandler// IMixedRealityFocusHandler 是實現 eyetracking 的接口
+{
+    protected static PointerEventData leftClickEventData => new PointerEventData(EventSystem.current);
+    public IPointerClickHandler clickTarget;//自身ui 組件
+     
+
+    
+    public void OnFocusEnter(FocusEventData eventData)//注視到物件 執行一次 
+    {
+        // Debug.Log("OnFocusEnter");
+        IPointerDownHandler pointerDown = clickTarget as IPointerDownHandler;//轉型成IPointerDownHandler
+        pointerDown?.OnPointerDown(leftClickEventData); //按下時顯示閃
+    }
+
+    public void OnFocusExit(FocusEventData eventData)//移開物件
+    {
+        //Debug.Log("OnFocusExit");
+        IPointerUpHandler pointerUp = clickTarget as IPointerUpHandler;
+        pointerUp?.OnPointerUp(leftClickEventData);//放開時顯示變回本來顏色
+        clickTarget?.OnPointerClick(leftClickEventData);//執行onclick方法
+    }
+    private void Awake()
+    {
+        clickTarget = GetComponent<IPointerClickHandler>();
+         
+    }
+
+
+}
---
+++ Assets/Jaosn/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]private Animator ani;
+    void Start()
+    {
+        ani = transform.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void PlayerAction() { 
+        ani.SetTrigger("action");
+    
+    }
+}
